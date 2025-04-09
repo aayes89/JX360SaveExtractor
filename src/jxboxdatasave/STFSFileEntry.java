2b1840f52cca911a2a390eca6a2aad299e46a61a
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2025-04-09 01:11:38 -0600

    Update STFSFileEntry.java

--- src/jxboxdatasave/STFSFileEntry.java
+++ src/jxboxdatasave/STFSFileEntry.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package jxboxdatasave;
 
 /**
@@ -15,7 +11,3 @@ class STFSFileEntry {
     int startingBlock;
     byte[] content;
 }
-
-// A partir de cierto offset, empieza la tabla de archivos y sus bloques de datos.
-// Implementar requiere parseo de hash tables y cadena de bloques.
-
